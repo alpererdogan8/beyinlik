5c3355d939d953593ee23feae406d0175066d787
Alper Erdogan <alpererdogan8@gmail.com>
2023-11-06 01:38:56 +0300

    fix: register problem solved

--- routes/auth.php
+++ routes/auth.php
@@ -12,11 +12,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register/reg7sf47si54fds75f4ds74fssds75gd4s75gsterd3g75d3g7sd', [RegisteredUserController::class, 'create']);
+    Route::get('register/reg7sf47si54fds75f4ds74fssds75gd4s75gsterd3g75d3g7sd', [RegisteredUserController::class, 'create'])->name("register");
     Route::get('register', function () {
         return redirect('/');
     });
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register/reg7sf47si54fds75f4ds74fssds75gd4s75gsterd3g75d3g7sd', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
