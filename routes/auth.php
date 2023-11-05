d72b4908aec1349cbba7ab9b9fd9037dcc3f3081
Alper Erdogan <alpererdogan8@gmail.com>
2023-11-06 01:18:14 +0300

    fix: fixed route

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register/passwordd7sf47s54fds75f4ds74fds75gd4s75gsd3g75d3g7sd', [RegisteredUserController::class, 'create']);
+    Route::get('register/reg7sf47si54fds75f4ds74fssds75gd4s75gsterd3g75d3g7sd', [RegisteredUserController::class, 'create']);
     Route::get('register', function () {
         return redirect('/');
     });
