cac723f7c7517bfb9e3b140d7196c568baa56a41
Alper Erdogan <alpererdogan8@gmail.com>
2023-11-06 03:20:01 +0300

    feat: endpoint extension nomenclature recreated

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [Content::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
     Route::get('/dashboard/content/create', [Content::class, 'create'])
         ->middleware(['auth', 'verified'])->name('create');
-    Route::post('/dashboard/content/create', [Content::class, 'store'])->middleware(['auth', 'verified'])->name('create');
+    Route::post('/dashboard/content/create', [Content::class, 'store'])->middleware(['auth', 'verified'])->name('create.post');
     Route::get('/dashboard/content/edit/{id}', [Content::class, 'edit'])->middleware(['auth', 'verified'])->name('edit');
     Route::post('/dashboard/content/edit/{id}', [Content::class, 'update'])->middleware(['auth', 'verified'])->name('update');
     Route::delete("/dashboard/content/delete/{id}", [Content::class, 'destroy'])->middleware(['auth', 'verified'])->name('delete');
