428f6f3e543caf374a4c179ded00502a320bd7ef
Alper Erdogan <alpererdogan8@gmail.com>
2023-11-06 03:28:42 +0300

    fix:fixing commit sent without build

--- public/build/assets/FormDataEditor-8c0ca9b3.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as r}from"./app-696690af.js";import{z as s}from"./index-33b420f3.js";function i({field:e}){const{editor:t}=s();return o.useEffect(()=>{e.onChange(t.getHTML())},[t.getHTML()]),r.jsx(r.Fragment,{})}export{i as default};
